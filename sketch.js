e2a6ac2f877ee7bfe7459475fbdb5e4d4ba781c7
Mormoreno <32307671+Mormoreno@users.noreply.github.com>
2017-09-28 11:24:19 +0200

    Correzione 1

--- sketch.js
+++ sketch.js
@@ -1,49 +1,50 @@
 function setup() {
-	createCanvas(500, 500);
-		background(100,100,100);
-
-
-	
+  createCanvas(800,600);
+  background(200,200,200);
 }
 
-var x=0;
 function draw() {
-
-	
-	x++;
-	colorMode(RGB);
-	strokeWeight(10);
-	stroke(50,50,50,x);
 	
 
-	//noStroke();
-
-
-	fill(210, 0, 0);
-	//noFill();
-
-
-	//line(x,y,x1,x1);
-	line(200, 200, 400, 400);
-
-	//rect(x,y,width,height); //x,y punto in alto a sinistra
-	rect(120, 120, 80, 80);
 	
-	colorMode(HSB);
-	fill(60,90,100,.5);
-
-	//ellipse(x,y,weight,height); x,y = centro
-	ellipse(250, 250, 100, 100);
-
-	//ellipse(x,y,weight,height); x,y = centro
-	ellipse(350, 350, 80, 80);
+	//Collo
+	strokeWeight(4);
+	stroke(110,110,110);
+	line(400,400,400,220);
+	line(415,400,415,220);
+	line(430,400,430,220);
 	
-	ellipse(width*.5,height/2,width/2,width/2);
+	//Testa
+	line(415,180,380,130);
+	line(415,180,440,60);
+	line(415,180,500,200);
+	noStroke();
+	fill(1,1,1);
+	ellipse(415,180,100,100);
 	
+	//Occhi
+	fill(255,255,255);
+	ellipse(430,170,30,30);
+	fill(0,0,0);
+	ellipse(430,170,6,6);
+	fill(200,200,200);
+	ellipse(405,165,8,8);
+	ellipse(450,180,5,5);
+	ellipse(440,148,6,6);
 	
-	
-	stroke(0,0,0);
-	line(pmouseX,pmouseY,mouseX,mouseY);
-	//	line(mouseX,mouseY,pmouseX,pmouseY);
-
+	//Corpo
+	fill(110,110,110);
+	ellipse(400,450,70,70);
+	fill(1,1,1);
+  	rect(350,300,100,150);
+  	fill(110,110,110);
+  	rect(350,320,100,10);
+  	
+  	//Logo
+  	strokeWeight(10);
+  	stroke(0,200,0);
+  	line(380,400,380,350);
+  	line(380,350,400,380);
+  	line(400,380,420,350);
+  	line(420,350,420,400);
 }
